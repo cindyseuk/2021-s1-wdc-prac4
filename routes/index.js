631d56cce4e1d4a6e9a1d63ed496d08fe10c61e4
Your Name <you@example.com>
2021-03-29 10:09:24 +0000

    update

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,14 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+var visits=0;
+router.get('/last.txt', function(req, res, next) {
+  visits++;
+  var date = new Date();
+  if (visits>1){
+    res.send(String(date));
+  }
+
+});
+
 module.exports = router;
